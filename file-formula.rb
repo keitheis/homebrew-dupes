d2640fb8b0a9a534db1e1ab6fc2503cdfeadace6
Jack Nagel <jacknagel@gmail.com>
2012-01-31 21:38:26 -0600

    fix audit warnings
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- file-formula.rb
+++ file-formula.rb
@@ -5,7 +5,7 @@ class FileFormula < Formula
   url 'ftp://ftp.astron.com/pub/file/file-5.08.tar.gz'
   homepage 'http://www.darwinsys.com/file/'
   md5 '6a2a263c20278f01fe3bb0f720b27d4e'
-  head 'git://github.com/glensc/file.git'
+  head 'https://github.com/glensc/file.git'
 
   def install
     system "./configure", "--disable-dependency-tracking",
