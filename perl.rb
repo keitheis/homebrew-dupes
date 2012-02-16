857f02e4ffc0bb4bab4374ca361a86399ebadbad
Gary Monson <gary.monson@gmail.com>
2012-02-16 22:57:56 +1100

    perl: updated to 5.14.2, made threads optional
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- perl.rb
+++ perl.rb
@@ -1,13 +1,40 @@
 require 'formula'
 
 class Perl < Formula
-  url 'http://www.cpan.org/src/5.0/perl-5.14.1.tar.gz'
+  url 'http://www.cpan.org/src/5.0/perl-5.14.2.tar.gz'
   homepage 'http://www.perl.org/'
-  md5 '0b74cffa3a10aee08442f950aecbaeec'
+  md5 '3306fbaf976dcebdcd49b2ac0be00eb9'
+
+  def options
+    [['--use-threads', 'Enable perl threads']]
+  end
+
+  def threads?
+      ARGV.include? '--use-threads'
+  end
 
   def install
-    system("rm -f config.sh Policy.sh");
-    system "sh Configure -de -Dprefix=#{prefix} -Dusethreads -Duseshrplib -Duselargefiles"
+    system("rm -f config.sh Policy.sh")
+    args = [
+        '-des',
+        "-Dprefix=#{prefix}",
+        "-Dman1dir=#{prefix}/share/man/man1",
+        "-Dman3dir=#{prefix}/share/man/man3",
+        '-Duseshrplib',
+        '-Duselargefiles',
+    ]
+    if threads?
+        args << '-Dusethreads'
+    end
+    system './Configure', *args
+    system "make"
+    system "make test"
     system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+      Builds without threads by default.  Use --use-threads to build with
+      threads.
+      EOS
   end
 end
