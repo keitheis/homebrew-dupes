71cf6a65df5d345023b6252def10f190b201f3de
Adam Vandenberg <flangy@gmail.com>
2012-03-15 19:57:56 -0700

    openldap: don't clean run folder

--- openldap.rb
+++ openldap.rb
@@ -1,20 +1,21 @@
 require 'formula'
 
 class Openldap < Formula
-  url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.24.tgz'
   homepage 'http://www.openldap.org/software/'
+  url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.24.tgz'
   md5 '116fe1e23a7b67686d5e62274367e6c0'
 
   depends_on 'berkeley-db' if ARGV.include? "--with-berkeley-db"
 
+  skip_clean 'var/run'
+
   def options
-    [
-      ["--with-berkeley-db", "compile openldap with berkeley-db support"],
-    ]
+    [["--with-berkeley-db", "compile openldap with berkeley-db support"]]
   end
 
   def install
-    args = ["--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"]
+    args = ["--disable-debug", "--disable-dependency-tracking",
+            "--prefix=#{prefix}"]
 
     args << "--enable-bdb=no" unless ARGV.include? "--with-berkeley-db"
     args << "--enable-hdb=no" unless ARGV.include? "--with-berkeley-db"
